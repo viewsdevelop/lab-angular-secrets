feab874004d6a13f76e9d8a1f1a7c1fff0e3f208
viewsdevelop <viewsdevelop@gmail.com>
2018-01-19 11:10:00 -0500

    Implement changes necessary to complete Iteration 7: Signup Component

--- starter-code/client/src/app/auth-signup/auth-signup.component.ts
+++ starter-code/client/src/app/auth-signup/auth-signup.component.ts
@@ -1,13 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
+// To Review
+import { SessionService } from "./../session.service";
+
+// To Review
+import { Router } from "@angular/router";
+
 @Component({
   selector: 'app-auth-signup',
   templateUrl: './auth-signup.component.html',
   styleUrls: ['./auth-signup.component.css']
 })
 export class AuthSignupComponent implements OnInit {
-  constructor() { }
+  
+  // To Review
+  user: any;
+  //
 
+  // Create the "formInfo" object to store all the information on
+  // the form.
+  formInfo = {
+    username: '',
+    password: '',
+    name: "",
+    secret: ""
+  };
+  
+  // To Review: Stores the error that can be generated during the 
+  // signup process.
+  error: string;
+  //
+  
+  // To Review
+  constructor(private session: SessionService, private router: Router) { }
+  //
+  
+  // To Review
   ngOnInit() {
+  }
+  //
+
+  // To Review: Signup Method to handle the request.
+  signup() {
+    this.session.signup(this.formInfo)
+      .subscribe(
+        (user) => {
+          this.user = user;
+          this.router.navigate(["/private"]);
+        },
+        (err) => {
+          this.error = err;
+        }
+      );
   }
 }
