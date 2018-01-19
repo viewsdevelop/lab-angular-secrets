0c2656ce44cbd35b131e4a1845294b0ceba2003f
viewsdevelop <viewsdevelop@gmail.com>
2018-01-19 11:19:26 -0500

    Implement changes to complete Iteration 8: Login Component

--- starter-code/client/src/app/auth-login/auth-login.component.ts
+++ starter-code/client/src/app/auth-login/auth-login.component.ts
@@ -1,13 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+// To Review
+import { SessionService } from "./../session.service";
+import { Router } from "@angular/router";
+//
+
 @Component({
   selector: 'app-auth-login',
   templateUrl: './auth-login.component.html',
   styleUrls: ['./auth-login.component.css']
 })
 export class AuthLoginComponent implements OnInit {
-  constructor() { }
+  // To Review
+  user: any;
+  //
+  
+  // To Review: formInfo object to store all the information of the form 
+  formInfo = {
+    username: '',
+    password: ''
+  };
+  error: string;
+  
+  // To Review
+  constructor(private session: SessionService, private router: Router) { }
+  //
 
   ngOnInit() {
+  }
+
+  // To Review: login method to handle the request.
+  login() {
+    this.sesssion.login(this.formInfo)
+      .subscribe(
+        (user) => { this.router.navigate(["/private"]); },
+        (err) => {
+          this.error = err;
+        }
+      );
   }
 }
